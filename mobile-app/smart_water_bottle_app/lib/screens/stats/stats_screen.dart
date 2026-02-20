055c9ad3fb0ea5ab7ec2c131ca38c0492285ddcc
ADHEESHA SOORIYAARACHCHI <106448404+adeshasur@users.noreply.github.com>
2026-02-20 12:57:35 +0530

    feat: setup basic layout and appbar for stats screen

---
+++ mobile-app/smart_water_bottle_app/lib/screens/stats/stats_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class StatsScreen extends StatefulWidget {
+  const StatsScreen({super.key});
+
+  @override
+  State<StatsScreen> createState() => _StatsScreenState();
+}
+
+class _StatsScreenState extends State<StatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'Hydration Stats 📊',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text('Stats Dashboard Loading...'), //
+      ),
+    );
+  }
+}
