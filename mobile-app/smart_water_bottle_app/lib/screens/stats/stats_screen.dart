ee6ded23a7b54d5ab5fe3c833eedc65047d6da38
ADHEESHA SOORIYAARACHCHI <106448404+adeshasur@users.noreply.github.com>
2026-02-21 10:12:43 +0530

    add reusable statCard widget

--- mobile-app/smart_water_bottle_app/lib/screens/stats/stats_screen.dart
+++ mobile-app/smart_water_bottle_app/lib/screens/stats/stats_screen.dart
@@ -20,14 +20,31 @@ class StatsScreen extends StatelessWidget {
               ),
               const SizedBox(height: 30),
             ],
-              final List weeklyIntake = [1800, 2100, 1600, 2400, 2000, 2300, 1900];
-          const double goal = 2500;
-          double average = weeklyIntake.reduce((a, b) => a + b) / weeklyIntake.length;
-          double bestDay = weeklyIntake.reduce((a, b) => a > b ? a : b);
           ),
         ),
       ),
     );
 
   }
+}
+final List weeklyIntake = [1800, 2100, 1600, 2400, 2000, 2300, 1900];
+const double goal = 2500;
+double average = weeklyIntake.reduce((a, b) => a + b) / weeklyIntake.length;
+double bestDay = weeklyIntake.reduce((a, b) => a > b ? a : b);
+
+Widget statCard({required String title, required String value, required Color color}) {
+  return Container(
+    padding: const EdgeInsets.all(20),
+    decoration: BoxDecoration(
+      color: const Color(0xFF1E293B),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Column(
+      children: [
+        Text(title, style: const TextStyle(color: Colors.white54)),
+        const SizedBox(height: 10),
+        Text(value, style: TextStyle(color: color, fontSize: 20, fontWeight: FontWeight.bold)),
+      ],
+    ),
+  );
 }
