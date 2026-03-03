7ad49033f3c912e2b1ec4593fa3560e6fb1a458f
ADHEESHA SOORIYAARACHCHI <106448404+adeshasur@users.noreply.github.com>
2026-03-04 00:20:43 +0530

    style: final UI polish and color adjustments for main dashboard

--- mobile-app/smart_water_bottle_app/lib/main.dart
+++ mobile-app/smart_water_bottle_app/lib/main.dart
@@ -12,9 +12,14 @@ class SmartWaterBottleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: const StatsScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF4F7FC),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const StatsScreen(),
     );
   }
 }
